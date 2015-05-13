16f563d8ebc97b0472c487193ef039f4fce9a799
Aakansha <monstershome@gmail.com>
2015-05-13 17:03:43 +0530

    Added deletion at a position in circular linklist

--- GEEKS/src/DataStructures/CircularLinkList.java
+++ GEEKS/src/DataStructures/CircularLinkList.java
@@ -1,38 +1,36 @@
 package DataStructures;
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-
 /**
  * @author Aakansha Doshi
  *
  */
 public class CircularLinkList {
-static Node start;
-	
+static Node start;	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		
-		CircularLinkList ob=new CircularLinkList();	
-		
+		// TODO Auto-generated method stub		
+		CircularLinkList ob=new CircularLinkList();			
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
-			System.out.print("\nEnter 1 for insertion\nEnter 2 for display\n");
+			System.out.print("\nEnter 1 for insertion at begining\nEnter 2 for display\nEnter 3 to delete");
 			int choice=Integer.parseInt(br.readLine().trim());
 			switch(choice)
 			{
 			case 1:
+				System.out.println("Enter the number to be inserted at begining ");
 				int num=Integer.parseInt(br.readLine().trim());				
-				ob.insert(num);
+				ob.insertbegin(num);
 				break;
 			case 2:
-				
 				ob.display();
 				break;
-			
+			case 3:
+				System.out.println("Enter the position to be deleted");
+				int pos=Integer.parseInt(br.readLine());
+				num=ob.delete(pos);
+				System.out.println("The deleted node is "+num);
+				break;			
 			default:
 				System.exit(0);;
 				
@@ -41,21 +39,67 @@ static Node start;
 		}
 
 	}
+	private int count()
+	{
+		Node p=start;
+		int c=0;
+		if(p!=null){
+			while(p.link!=start)
+			{
+				c++;
+				p=p.link;
+			}
+			c++;
+		}
+		return c;
+	}
+	private int  delete(int pos) {
+	//Considering the first node inseretd as the starting position		
+		Node p=start;int del;
+		int i=2;
+		if(pos==1)		
+		{
+			pos=count()+1;
+			System.out.println("pos= "+pos);
+			start=p.link;
+			if(pos==2)
+				start=null;
+		}
+		while(i<pos)
+		{
+			p=p.link;
+			i++;
+		}
+		del=p.link.data;
+		p.link=p.link.link;
+		if(pos==count()+1)
+		{
+			start=p;
+		}
+		return del;
+		
+		
+	}
 
 	private  void display() {
 		// TODO Auto-generated method stub
 		Node p=start;
 		//System.out.println(start.data);
+		if(p!=null){
 		while(p.link!=start)
 		{
 			System.out.print(p.data+"-->");
 			p=p.link;
 		}
-		System.out.print(p.link.data+"\n");
-		
+		System.out.print(p.data+"\n");
+		}
+		else
+		{
+			System.out.println("Empty");
+		}
 	}
 
-	private  void insert(int num) {
+	private  void insertbegin(int num) {
 		// TODO Auto-generated method stub
 		Node p=new Node();
 		p.data=num;
@@ -63,6 +107,7 @@ static Node start;
 		Node temp=start;
 		if(start==null)
 		{
+			System.out.println("inserted first");
 			p.link=p;
 		}
 		else
@@ -74,7 +119,7 @@ static Node start;
 			temp.link=p;
 		}
 		start=p;
-		System.out.println(start.data);
+		//System.out.println(start.data);
 		
 		
 		

030fbbb69064f1d6c91d49bc3c3ab2fb528b1f03
Aakansha <monstershome@gmail.com>
2015-07-28 01:37:47 +0530

    code formatted

--- GEEKS/src/Dynamic/knapsack01/Recursive_Sol.java
+++ GEEKS/src/Dynamic/knapsack01/Recursive_Sol.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 public class Recursive_Sol {
 
 	public static void main(String[] args) throws IOException {
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));	
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));	
 		
 		int n=Integer.parseInt(br.readLine());
 		String value=br.readLine();
